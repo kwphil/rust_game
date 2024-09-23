1f7536bc252c95892066cfbecbb9694687b3a14f
Folkorr <bigboxerbabies@gmail.com>
2024-09-23 10:55:26 -0500

    Update platform.rs

--- src/platform.rs
+++ src/platform.rs
@@ -1,1 +1,14 @@
-
+struct platform;
+commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.2, 0.8, 0.2),
+                custom_size: Some(Vec2::new(200.0, 30.0)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(0.0, -100.0, 0.0), // Position below the player
+            ..Default::default()
+        },
+        Platform,
+    ));
+}
