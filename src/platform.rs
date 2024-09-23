fc44a4535acf900afac40bdcecb5232b7842f8ea
kwphil <121273949+kwphil@users.noreply.github.com>
2024-09-23 11:29:56 -0500

    Update platform.rs

--- src/platform.rs
+++ src/platform.rs
@@ -1,15 +1,27 @@
 use bevy::prelude::*;
-pub struct platform;
-commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(0.2, 0.8, 0.2),
-                custom_size: Some(Vec2::new(800.0, 50.0)),
-                ..Default::default()
-            },
-            transform: Transform::from_xyz(0.0, -100.0, 0.0), // Position below the player
-            ..Default::default()
-        },
-        Platform,
-    ));
+
+pub struct PlatformPlugin;
+
+impl Plugin for PlatformPlugin {
+        pub fn build(&self, app: &mut App) {
+                app.add_systems(Update, spawn_platform);
+}
+
+#[derive(Component)]
+pub struct Platform;
+
+fn spawn_platform(mut commands: Commands) {
+        commands.spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::rgb(0.2, 0.8, 0.2),
+                        custom_size: Some(Vec2::new(800.0, 50.0)),
+                        ..Default::default()
+                    },
+                    transform: Transform::from_xyz(0.0, -100.0, 0.0), // Position below the player
+                    ..Default::default()
+                },
+                Platform,
+            ));
+        }
 }
