67fc0a982541499da90a6a7f39f01e221b7241a0
kwphil <121273949+kwphil@users.noreply.github.com>
2024-09-23 13:01:51 -0500

    Update weapon.rs

--- src/weapon.rs
+++ src/weapon.rs
@@ -4,12 +4,22 @@ mod ammo;
 
 #[derive(Component)]
 pub struct Weapon {
-  weapon_id: u8,
+  weapon_id: WeaponType,
   ammo: ammo::Ammo,
+  damage: u8,
+  range: f32,
+  reload: f32,
 }
 
 impl Weapon {
-  pub fn new() {
+  pub fn melee() {
     
   }
+}
+
+pub struct WeaponInfo {
+  name: String,
+  range: f32,
+  damage: u8,
+  reload: f32,
 }
