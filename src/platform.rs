6c8d07e0a915cd5765b84420bd7e5a69ff1960e4
Folkorr <bigboxerbabies@gmail.com>
2024-09-23 10:58:35 -0500

    Update platform.rs
    Platform

--- src/platform.rs
+++ src/platform.rs
@@ -4,7 +4,7 @@ commands.spawn((
         SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb(0.2, 0.8, 0.2),
-                custom_size: Some(Vec2::new(200.0, 30.0)),
+                custom_size: Some(Vec2::new(800.0, 50.0)),
                 ..Default::default()
             },
             transform: Transform::from_xyz(0.0, -100.0, 0.0), // Position below the player
