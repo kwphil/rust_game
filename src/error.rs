564bd43a8f6d2c7d27dfa4fdf9ac0cb2b8a57e5b
kwphil <121273949+kwphil@users.noreply.github.com>
2024-09-23 09:05:18 -0500

    Create error.rs

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+pub struct Res<'a, T, E> {
+    t: Option<&'a T>,
+    e: Option<&'a E>,
+}
+
+impl<'a, T, E> Res<'a, T, E> {
+    pub fn unwrap(&self) -> T {
+        if e != None {
+            panic!("{}", e);
+        }
+    }
+
+    pub fn unwrap_or_else<F>(&self, f: F) -> T 
+    where
+        Fn(E) -> ()
+    {
+        if e != None {
+            f(self.e)
+        }
+    }
+}
+
+pub fn Err<E>(e: E) -> Res<(), E> {
+    Res<(), E> {
+        t: None,
+        e,
+    }
+}
+
+pub fn Ok<T>(t: T) -> Res<T, ()> {
+    Res<T, ()> {
+        t,
+        e: None,
+    }
+}
